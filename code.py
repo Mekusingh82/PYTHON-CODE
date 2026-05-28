c93b09e85fb5a0020c338caea0a1c50f8132def0
Mekusingh82 <mekukumari29@gmail.com>
2026-05-28 20:07:16 +0530

    Introduction

---
+++ code.py
@@ -0,0 +1,15 @@
+name="Meku Singh"
+age="18"
+hight="4.10"
+course="BCA"
+gender="female"
+address="Chapra Bihar Manpur 841311"
+collage="Oxford Collage of Research and Management Fatuha BranchS"
+
+print(f"{name}")
+print(age)
+print(hight)
+print(course)
+print(gender)
+print(address)
+print(collage)
